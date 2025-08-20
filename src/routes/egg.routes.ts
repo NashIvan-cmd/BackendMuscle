c1893a68bb86d833ab46fadfd9d2ed1acd1d5a28
NashIvan-cmd <nashivanramos@gmail.com>
2025-08-20 12:52:55 +0800

    added egg routes

--- src/routes/egg.routes.ts
+++ src/routes/egg.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { verifyAccessToken } from "../middleware/auth.middleware";
-import { createEgg } from "../controllers/egg.controller";
+import { createEgg, orderEgg, getAllEgg, deleteEgg } from "../controllers/egg.controller";
 
 const router = Router();
 
-router.post('/egg', verifyAccessToken, createEgg);
+router.post('/v1/egg', verifyAccessToken, createEgg);
+router.post('/v1/order/egg', verifyAccessToken, orderEgg);
+
+router.get('/v1/egg', verifyAccessToken, getAllEgg);
+
+router.delete('/v1/egg', verifyAccessToken, deleteEgg);
 
 export default router;
